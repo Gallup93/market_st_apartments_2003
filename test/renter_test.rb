098824e7e59b0cc5403e542220d790379afdce97
Michael Gallup <Gallup93@icloud.com>
2020-03-31 09:11:16 -0600

    added renter initialization and corresponding test method

---
+++ test/renter_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+# require './lib/apartment'
+
+class RenterTest < Minitest::Test
+
+  def test_initialization
+
+    renter1 = Renter.new("Jessie")
+    assert_instance_of Renter, renter1
+
+  end
+
+
+
+end
