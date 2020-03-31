a600395d605dd96abae83681c5af5f4b48d898ee
Michael Gallup <Gallup93@icloud.com>
2020-03-31 09:22:05 -0600

    added assert to initialization test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/renter'
-# require './lib/apartment'
+require './lib/apartment'
 
 class RenterTest < Minitest::Test
 
@@ -9,7 +9,7 @@ class RenterTest < Minitest::Test
 
     renter1 = Renter.new("Jessie")
     assert_instance_of Renter, renter1
-
+    assert_equal "Jessie", renter1.name
   end
 
 
