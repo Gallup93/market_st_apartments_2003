e01a88fe46896e5f6b0a3bff418f3d1962492582
Michael Gallup <Gallup93@icloud.com>
2020-03-31 09:31:52 -0600

    added add_renter method with corresponding test method

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -12,4 +12,13 @@ class TestApartment < Minitest::Test
     assert_equal 1, unit1.bathrooms
     assert_equal 1, unit1.bedrooms
   end
+
+
+  def test_add_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    assert_nil nil, unit1.renter
+    unit1.add_renter(renter1)
+    assert_instance_of Renter, unit1.renter
+  end
 end
